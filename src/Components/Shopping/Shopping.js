73f8e0ffb278587a05e15d8f8d2366cdbd3f21ee
niloy143 <niloymahmudapu@gmail.com>
2022-09-25 15:28:18 +0600

    price count

--- src/Components/Shopping/Shopping.js
+++ src/Components/Shopping/Shopping.js
@@ -2,17 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './Shopping.css';
 
 const Shopping = () => {
+    const [selectedItems, setSelectedItems] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(0);
+
+    const addToCart = ({price}) => {
+        setSelectedItems(selectedItems + 1);
+        setTotalPrice(totalPrice + price);
+    }
+
     return (
         <div className='shopping'>
-            <DisplayShop></DisplayShop>
-            <OrderSummery></OrderSummery>
+            <DisplayShop addToCart={addToCart}></DisplayShop>
+            <OrderSummery selectedItems={selectedItems} totalPrice={totalPrice}></OrderSummery>
         </div>
     );
 };
 
 
 // Display items here
-const DisplayShop = () => {
+const DisplayShop = props => {
     const [items, setItems] = useState([]);
     useEffect(() => {
         fetch('products.json')
@@ -22,14 +30,14 @@ const DisplayShop = () => {
 
     return (
         <div className='grid grid-cols-2 xl:grid-cols-3 gap-12 px-24 py-28'>
-            {items.map(item => <Items key={item.id} item={item}></Items>)}
+            {items.map(item => <Items key={item.id} item={item} addToCart={props.addToCart}></Items>)}
         </div>
     )
 }
 
 // individual items
 const Items = props => {
-    const { img, name, price, seller, ratings } = props.item;
+    const { img, name, price, seller, ratings} = props.item;
     return (
         <div className='border-2 border-gray-400 rounded-lg overflow-hidden flex flex-col justify-between'>
             <div>
@@ -44,7 +52,7 @@ const Items = props => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings > 1 ? ratings + ' Stars' : ratings + ' Star'}</p>
                 </div>
-                <button className='bg-orange-200 w-full py-3 text-lg font-semibold border-t-2 border-gray-400'>Add to Cart</button>
+                <button className='bg-orange-200 active:bg-orange-300 w-full py-3 text-lg font-semibold border-t-2 border-gray-400' onClick={() => props.addToCart(props.item)}>Add to Cart</button>
             </div>
         </div>
     )
@@ -56,20 +64,21 @@ const Items = props => {
 
 
 // Order Summer Part
-const OrderSummery = () => {
+const OrderSummery = ({selectedItems, totalPrice}) => {
+    
     return (
-        <div className='bg-orange-200 text-slate-700'>
+        <div className='bg-orange-200 text-slate-700 sticky top-0 right-0 h-screen'>
             <h1 className='text-center py-12 text-3xl font-semibold'>Order Summary</h1>
             <div className='px-8 pb-12 text-lg font-semibold flex flex-col gap-3'>
-                <p>Selected Items: <span>0</span></p>
-                <p>Total Price: $<span>0</span></p>
-                <p>Total Shipping Charge: $<span>0</span></p>
-                <p>Tax: $<span>0</span></p>
-                <h2 className='text-2xl'>Grand Total: $<span>0</span></h2>
+                <p>Selected Items: <span>{selectedItems}</span></p>
+                <p>Total Price: $<span>{totalPrice}</span></p>
+                <p>Total Shipping Charge: $<span>{selectedItems*30}</span></p>
+                <p>Tax: $<span>{totalPrice/10}</span></p>
+                <h2 className='text-2xl'>Grand Total: $<span>{totalPrice+(selectedItems*30)+(totalPrice/10)}</span></h2>
             </div>
             <div className='flex flex-col gap-2 px-5'>
-                <button className='bg-red-600 text-white px-5 py-2 text-lg font-semibold rounded-md'>Clear Cart</button>
-                <button className='bg-yellow-600 text-white px-5 py-2 text-lg font-semibold rounded-md'>Review Order</button>
+                <button className='bg-red-600 active:bg-red-700 text-white px-5 py-2 text-lg font-semibold rounded-md'>Clear Cart</button>
+                <button className='bg-yellow-600 active:bg-yellow-700 text-white px-5 py-2 text-lg font-semibold rounded-md'>Review Order</button>
             </div>
         </div>
     )
