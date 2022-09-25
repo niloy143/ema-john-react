c347a7cb7ce3e056cfec4a94054854d3cf9d19d9
niloy143 <niloymahmudapu@gmail.com>
2022-09-25 14:04:51 +0600

    order summery

--- src/Components/Shopping/Shopping.js
+++ src/Components/Shopping/Shopping.js
@@ -42,7 +42,7 @@ const Items = props => {
             <div>
                 <div className='p-4'>
                 <p>Manufacturer: {seller}</p>
-                <p>Rating: {ratings}</p>
+                <p>Rating: {ratings > 1 ? ratings + ' Stars' : ratings + ' Star'}</p>
                 </div>
                 <button className='bg-orange-200 w-full py-3 text-lg font-semibold border-t-2 border-gray-400'>Add to Cart</button>
             </div>
@@ -58,8 +58,19 @@ const Items = props => {
 // Order Summer Part
 const OrderSummery = () => {
     return (
-        <div className='bg-orange-200'>
-            <p>This is order summery</p>
+        <div className='bg-orange-200 text-slate-700'>
+            <h1 className='text-center py-12 text-3xl font-semibold'>Order Summary</h1>
+            <div className='px-8 pb-12 text-lg font-semibold flex flex-col gap-3'>
+                <p>Selected Items: <span>0</span></p>
+                <p>Total Price: $<span>0</span></p>
+                <p>Total Shipping Charge: $<span>0</span></p>
+                <p>Tax: $<span>0</span></p>
+                <h2 className='text-2xl'>Grand Total: $<span>0</span></h2>
+            </div>
+            <div className='flex flex-col gap-2 px-5'>
+                <button className='bg-red-600 text-white px-5 py-2 text-lg font-semibold rounded-md'>Clear Cart</button>
+                <button className='bg-yellow-600 text-white px-5 py-2 text-lg font-semibold rounded-md'>Review Order</button>
+            </div>
         </div>
     )
 }
